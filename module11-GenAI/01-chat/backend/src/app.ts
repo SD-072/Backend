abe01f3b4b9b733673e918eed2cefa4a66ba1639
Renke Brixel <renke.brixel@gmail.com>
2026-04-16 13:58:00 +0200

    the chat beginnings

--- module11-GenAI/01-chat/backend/src/app.ts
+++ module11-GenAI/01-chat/backend/src/app.ts
@@ -3,12 +3,30 @@ import type { ErrorRequestHandler } from 'express';
 import express from 'express';
 
 import mongoose from 'mongoose';
+import OpenAI from 'openai';
 
 const mongoUri = process.env.MONGO_URI;
 if (!mongoUri) {
   throw new Error('MONGO_URI environment variable is not set');
 }
 await mongoose.connect(mongoUri, { dbName: 'chat' });
+
+const Chat = mongoose.model(
+  'chat',
+  new mongoose.Schema({
+    history: {
+      type: Array,
+      default: [],
+    },
+  }),
+);
+
+const client = new OpenAI({
+  apiKey: process.env.GOOGLE_GENERATIVE_AI_API_KEY,
+  baseURL: 'https://generativelanguage.googleapis.com/v1beta/openai/',
+});
+// const client = new OpenAI();
+// const client = new OpenAI({ apiKey: 'ollama', baseURL: 'http://127.0.0.1:11434/v1' });
 
 const port = process.env.PORT || 8080;
 
@@ -19,6 +37,54 @@ app.use(express.json());
 
 app.get('/', (_req, res) => {
   res.json({ message: 'Running' });
+});
+
+app.post('/messages', async (req, res) => {
+  const { prompt, chatId } = req.body;
+
+  let chat;
+
+  if (!chatId) {
+    chat = await Chat.create({ history: [] });
+  } else {
+    chat = await Chat.findById(chatId);
+  }
+  // console.log('Chat', chat);
+
+  if (!chat) {
+    throw Error('Chat not found', { cause: { status: 404 } });
+  }
+  const userMessage = { role: 'user', content: prompt };
+
+  const result = await client.chat.completions.create({
+    model: 'gemini-3.1-flash-lite-preview',
+    messages: [
+      // { role: 'system', content: 'answer briefly' },
+      ...chat.history,
+      userMessage,
+    ],
+    // reasoning_effort: 'minimal',
+    // temperature: 0.7,
+    // max_completion_tokens: 400,
+    // verbosity: 'low'
+  });
+
+  const answer = result.choices[0]?.message;
+
+  if (!answer) {
+    throw Error('Mo answer returned');
+  }
+
+  chat.history = [...chat.history, userMessage, answer];
+
+  await chat.save();
+
+  res.json({ result: answer.content, chatId: chat._id });
+});
+
+app.post('/images', async (req, res) => {
+  const { prompt } = req.body;
+  // ...
 });
 
 app.use('/{*splat}', () => {
